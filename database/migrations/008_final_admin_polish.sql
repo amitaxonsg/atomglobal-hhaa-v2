916cefaa14a3ed566549f4fc9b74d89c508ce841
amitaxonsg <am@1proit.com>
2026-07-19 17:20:22 +0800

    Limit transparent logo migration to browser branding

--- database/migrations/008_final_admin_polish.sql
+++ database/migrations/008_final_admin_polish.sql
@@ -2,23 +2,13 @@ SET NAMES utf8mb4;
 
 UPDATE global_settings
 SET setting_value = '/media/brand/atom-global-wordmark-transparent.svg', updated_at = NOW()
-WHERE setting_key IN ('branding.logo_url', 'branding.email_logo_url', 'branding.report_logo_url')
+WHERE setting_key = 'branding.logo_url'
   AND setting_value = '/media/brand/atom-global-wordmark.png';
 
 UPDATE branding_revisions
 SET settings_json = JSON_SET(settings_json, '$.logoUrl', '/media/brand/atom-global-wordmark-transparent.svg'),
     updated_at = NOW()
 WHERE JSON_UNQUOTE(JSON_EXTRACT(settings_json, '$.logoUrl')) = '/media/brand/atom-global-wordmark.png';
-
-UPDATE branding_revisions
-SET settings_json = JSON_SET(settings_json, '$.emailLogoUrl', '/media/brand/atom-global-wordmark-transparent.svg'),
-    updated_at = NOW()
-WHERE JSON_UNQUOTE(JSON_EXTRACT(settings_json, '$.emailLogoUrl')) = '/media/brand/atom-global-wordmark.png';
-
-UPDATE branding_revisions
-SET settings_json = JSON_SET(settings_json, '$.reportLogoUrl', '/media/brand/atom-global-wordmark-transparent.svg'),
-    updated_at = NOW()
-WHERE JSON_UNQUOTE(JSON_EXTRACT(settings_json, '$.reportLogoUrl')) = '/media/brand/atom-global-wordmark.png';
 
 INSERT INTO media_library (
   file_name, storage_path, mime_type, file_size, width, height,
