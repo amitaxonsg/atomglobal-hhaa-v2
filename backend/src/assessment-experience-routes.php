cd0712e483e4bc22b1f06e23e79d3d914ad73a34
amitaxonsg <am@1proit.com>
2026-07-20 04:49:02 +0800

    Add questionnaire experience public and admin routes

---
+++ backend/src/assessment-experience-routes.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use AtomGlobal\Http\Request;
+use AtomGlobal\Http\Response;
+
+$router->add('GET', '/api/public/assessment-experience', function () use ($container) {
+    return Response::json($container['assessmentExperience']->publicConfiguration());
+});
+
+$router->add('GET', '/api/admin/assessment-tracks/{id}/experience', function (Request $request, array $params) use ($auth, $container) {
+    $auth->requirePermission('assessments.manage');
+    return Response::json($container['assessmentExperience']->byTrackId((int) $params['id']));
+});
+
+$router->add('PUT', '/api/admin/assessment-tracks/{id}/experience', function (Request $request, array $params) use ($auth, $container, $csrf) {
+    $user = $auth->requirePermission('assessments.manage');
+    $csrf($request);
+    return Response::json($container['assessmentExperience']->save((int) $params['id'], $request->body, (int) $user['id']));
+});
