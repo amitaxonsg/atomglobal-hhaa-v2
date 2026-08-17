2fd3a57381ccbe4c990ca69bd3e4da2b5fa0905a
amitaxonsg <am@1proit.com>
2026-08-17 11:10:20 +0800

    Load questionnaire UAT layout stabilization

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./admin-security-polish.css";
 import "./admin-support.css";
 import "./questionnaire-process.css";
 import "./questionnaire-latest.css";
+import "./uat-layout-fix.css";
 import "./report-flow.css";
 
 function ServiceWorkerUpdate() {
